aee59aad64614fc35722bc2721482dcaad3ccfa7
Myriam <myriam.rihani00@gmail.com>
2021-11-12 16:06:42 +0100

    turn led on & off

--- Arduino_Drugminder/src/main.cpp
+++ Arduino_Drugminder/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#define onboard 13
 
 void setup() {
+  pinMode(onboard,OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  digitalWrite(onboard, LOW);
+  delay(1000);
+  digitalWrite(onboard,HIGH);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
