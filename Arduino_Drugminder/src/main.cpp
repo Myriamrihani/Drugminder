ab34a080aa90a87b818dfa902171925db0023e05
Myriam <myriam.rihani00@gmail.com>
2021-11-14 19:55:14 +0100

    Testing folder

--- Arduino_Drugminder/src/main.cpp
+++ Arduino_Drugminder/src/main.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include "Keyboard.h"
 #include "Pills.h"
 #define onboard 13
 
 void setup() {
   pinMode(onboard,OUTPUT);
   Serial.begin(9600);
-  Keyboard.begin();
+
   // put your setup code here, to run once:
-  key_pressed = false;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if
+  
 }
