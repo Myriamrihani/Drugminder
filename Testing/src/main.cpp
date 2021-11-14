8549792d23bb67708690443cae21fff26647b1e7
Myriam <myriam.rihani00@gmail.com>
2021-11-14 19:58:35 +0100

    several prescriptions at a time

--- Testing/src/main.cpp
+++ Testing/src/main.cpp
@@ -9,22 +9,29 @@ int main(){
 
     Prescription pre;
 
-    cout << "Do you want to add a medication?\n" << endl;
+    cout << "Do you want to add Prescription?\n" << endl;
 
     cin >> com;
 
     if(com == "yes")
     {
-        string name;
-        int r, nb;
-        cout << "Pill name?" << endl;
-        cin >> name;
-        cout << "Rack number inserted?" << endl;
-        cin >> r;
-        cout << "Number of pills added?" << endl;
-        cin >> nb;
+        int med;
+        cout << "How many medication to add? " << endl;
+        cin >> med;
+        
+        for(int i = 0; i< med; i++){
+            string name;
+            int r, nb;
+            cout << "Pill name?" << endl;
+            cin >> name;
+            cout << "Rack number inserted?" << endl;
+            cin >> r;
+            cout << "Number of pills added?" << endl;
+            cin >> nb;
 
-        pre.add_pill(name, r, nb);
+            pre.add_pill(name, r, nb);
+        }
+
         
         pre.print_prescription();
 
