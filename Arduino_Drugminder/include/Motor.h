6045cd071e9fbfd268a0cb7399b2f22e831d26a2
Myriam <myriam.rihani00@gmail.com>
2021-11-30 14:23:59 +0100

    motor

---
+++ Arduino_Drugminder/include/Motor.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include<Servo.h>
+
+extern Servo servo;
+extern const int dirPin_X;
+extern const int stepPin_X;
+extern const int dirPin_Y;
+extern const int stepPin_Y;
+extern const int step_round;
+extern int x_cor;
+extern int y_cor;
+extern int x_box;
+extern int y_box;
+
+void trialF();
---
+++ Arduino_Drugminder/src/trial.cpp
@@ -0,0 +1,22 @@
+
+// #include<Servo.h>
+// #include <Motor.h>
+
+// void setup()
+// {
+//  Serial.begin(9600);
+
+//   pinMode(stepPin_X, OUTPUT);
+//   pinMode(dirPin_X, OUTPUT);
+//     pinMode(stepPin_Y, OUTPUT);
+//   pinMode(dirPin_Y, OUTPUT);
+      
+// }
+// void loop()
+// {
+//    trialF();
+
+
+
+//   while(1){}
+// }
